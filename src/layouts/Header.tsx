fff68c4b4ee04f0b3222beb97a25595d0e96dc4a
Nguyen Viet <nguyenviet02.dev@gmail.com>
2025-01-24 16:32:30 +0700

    feat: style header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -44,7 +44,7 @@ export default function Example() {
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
           >
             <span className="sr-only">Open main menu</span>
-            <img src={Bar3Icon} alt="Icon" className="size-6" />
+            <img src={Bar3Icon} alt="Icon" className="filter-white size-6" />
           </button>
         </div>
         <div className="hidden h-[2.625rem] rounded-[30px] bg-gradient-to-r from-[#2250B5] to-[#289BE3] lg:flex">
@@ -56,7 +56,7 @@ export default function Example() {
               사전등록
             </a>
           </button>
-          <div className="h-full w-[0.3px] bg-white -skew-x-[18deg]"></div>
+          <div className="h-full w-[0.3px] -skew-x-[18deg] bg-white"></div>
           <button className="flex h-full w-[8.125rem] items-center justify-center">
             <a
               href="#"
@@ -110,7 +110,13 @@ export default function Example() {
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
-                  Log in
+                  사전등록
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  연락처
                 </a>
               </div>
             </div>
