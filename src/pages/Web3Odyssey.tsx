182ea3baef3a12c85f07eb3e731ced9e1dbc50be
Nguyen Viet <nguyenviet02.dev@gmail.com>
2025-02-11 09:47:09 +0700

    refactor: sponsor section

--- src/pages/Web3Odyssey.tsx
+++ src/pages/Web3Odyssey.tsx
@@ -1,9 +1,9 @@
 import Reference from "@components/common/Reference";
+import Sponsor from "@components/common/Sponsor";
 import Benefits from "@components/web3-odyssey/Benefits";
 import EventHighlight from "@components/web3-odyssey/EventHighlight";
 import Hero from "@components/web3-odyssey/Hero";
 import PreRegistration from "@components/web3-odyssey/PreRegistration";
-import Sponsor from "@components/web3-odyssey/Sponsor";
 
 const Web3Odyssey = () => {
   return (
